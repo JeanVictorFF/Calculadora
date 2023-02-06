1788804d301a8931210dfbbb4c4bd9c081bcf28f
Jean Victor <Victorferreira.franco86@gmail.com>
2023-02-06 14:07:57 -0300

    adicionei mais no JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -7,4 +7,54 @@ class Calculator {
         this.previousOperationText = previousOperationText
         this.currentOperationText = currentOperationText
         this.currentOperation = ""
-    }}
+    } 
+    addDigit(digit) {
+        
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+
+    processOperation(operation) {
+        
+        let operationValue;
+        let previous = +this.previousOperationText.innerText;
+        let current = +this.currentOperationText.innerText;
+
+        switch (operation) {
+            case "+": 
+            operationValue = previous + current;
+            this.updateScreen(operationValue, operation, current, previous);
+
+
+
+               
+                
+        }
+
+    }
+
+    updateScreen(
+        operationValue = null,
+        operation = null,
+        current = null,
+        previous = null
+        ) {
+            console.log(operationValue, operation, current, previous);
+
+        this.currentOperationText.innerText += this.currentOperation;
+    }
+} 
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === ".") {
+            calc.addDigit(value);
+        } else {
+            calc.processOperation(value);
+        }
+    });
+});
